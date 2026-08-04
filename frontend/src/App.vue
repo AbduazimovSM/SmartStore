cb2f04b1e4fdb3b9dea2f3be265332f402db6a9e
AbduazimovSM <abduazimov2002@gmail.com>
2026-08-04 14:20:35 +0500

    установил шаблон на проект smartstore

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,7 @@
-<template>
-  <ProductCard v-for="product in products" :key="product.id" :name="product.name" :price="product.price"/>
+<script setup></script>
 
+<template>
+    <router-view />
 </template>
 
-<script setup>
-  import ProductCard from './components/ProductCard.vue';
-
-  const products = [
-    {
-      name: "Iphone 15",
-      price:1500
-    },
-    {
-      name: "Iphone 16",
-      price:1600
-    },
-    {
-      name: "Iphone 17",
-      price:1700
-    }
-  ]
-
-
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
