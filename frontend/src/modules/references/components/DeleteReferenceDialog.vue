d4549ba5d3c29258fce7f261fa586bee0b10e068
AbduazimovSM <abduazimov2002@gmail.com>
2026-08-22 22:20:42 +0500

    +++++++++++++**************++++++++++++++

--- frontend/src/modules/references/components/DeleteReferenceDialog.vue
+++ frontend/src/modules/references/components/DeleteReferenceDialog.vue
@@ -1,18 +1,21 @@
 <template>
-    <Dialog v-model:visible="visible" :style="{ width: '450px' }" header="Подтверждение" modal>
+    <Dialog v-model:visible="visible" :style="{ width: '450px' }" :header="t('global.toast.confirmation')" modal>
         <div class="confirmation-content">
             <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem"/>
-            <span> Удалить <b>{{ itemName }}</b>? </span>
+            <span>{{ t('global.messages.confirm_delete', { name: itemName }) }}</span>
         </div>
 
         <template #footer>
-            <Button label="Нет" icon="pi pi-times" text :disabled="loading" @click="visible = false"/>
-            <Button label="Да" icon="pi pi-check" text :loading="loading" :disabled="loading" @click="emit('confirm')"/>
+            <Button :label="t('global.buttons.no')" icon="pi pi-times" text :disabled="loading" @click="visible = false"/>
+            <Button :label="t('global.buttons.yes')" icon="pi pi-check" text :loading="loading" :disabled="loading" @click="emit('confirm')"/>
         </template>
     </Dialog>
 </template>
 
 <script setup>
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
+
 const visible = defineModel({
     type: Boolean,
     default: false
